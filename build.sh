d67c6f4f3c0d678596d3a7043ec505b0d4e938d9
vnkthegoate <goatevnkthegoate@gmail.com>
2025-08-01 11:11:47 -0300

    Atualizar o build.sh

--- build.sh
+++ build.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
-# Navega para o diretório
+# Instala as dependências de build necessárias para compilar o Whisper.cpp
+# Este comando é essencial e deve resolver o seu problema
+sudo apt-get update
+sudo apt-get install -y build-essential
+
+# Navega para o diretório do Whisper.cpp
 cd whisper.cpp
 
-# Compila o executável do Whisper
-make
-
-# Navega para a pasta de modelos
-cd models
+# Compila o executável do Whisper.
+# Adicionamos 'gmake' como alternativa a 'make'
+make || gmake
 
 # Baixa e converte o modelo ggml-small.bin
+cd models
 ./make-ggml.sh small
