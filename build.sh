6b9918c5ff2673f722c69fd70934a736d1ed4a69
vnkthegoate <goatevnkthegoate@gmail.com>
2025-08-01 11:38:22 -0300

    Atualizar o build.sh

--- build.sh
+++ build.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
 
+echo "Compilando Whisper.cpp..."
 # Navega para o diretório
 cd whisper.cpp
-
-# Instala o CMake, se necessário
-apt-get update
-apt-get install -y cmake
 
 # Compila o executável do Whisper
 cmake -Bbuild
 make -Cbuild
 
-# Navega para a pasta de modelos
-cd models
-
 # Baixa e converte o modelo ggml-small.bin
+cd models
 ./make-ggml.sh small
